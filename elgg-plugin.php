58a5edbfd24c1d8ccaca662dc6b2a0bc5f70c82d
Jerôme Bakker <jeabakker@coldtrick.com>
2026-07-30 11:06:53 +0200

    chore: wrapping up v10.0.2

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -9,7 +9,7 @@ require_once(__DIR__ . '/lib/functions.php');
 return [
 	'plugin' => [
 		'name' => 'OpenSearch',
-		'version' => '10.0.1',
+		'version' => '10.0.2',
 		'dependencies' => [
 			'search' => [
 				'position' => 'after',
